c2c760b29a82240d417d85cc6492827bf18ebc28
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-22 04:48:13 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -12,8 +12,8 @@ def hello(array)
 end
 
 #Marcel Galang tried doing this ^
-#Instead of explaining to me -> He tried moving me forward 
-#because the answers were the next lesson.
+#Instead of explaining to me -> He tried moving me forward
+#because the answers were the next lesson.exit
 
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
